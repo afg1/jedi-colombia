9d3478f538706f213866f99ef7e21a41065f16d8
Andrew Green <andrew.green-2@manchester.ac.uk>
2019-02-01 11:09:55 +0000

    Script to create linux users in the container

---
+++ addUsersWithPasswords.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## This bash script reads a list of usernames and passwords from a file
+## and adds the users with the given password
+##
+## We expect the format of the file to be like this:
+## <username> <password>\n
+
+while read us
+do
+    uname="$(cut -d' ' -f1 <<<"$us")"
+    pword="$(cut -d' ' -f2 <<<"$us")"
+    useradd -rm -d /home/$uname -p "$(openssl passwd -1 $pword)" $uname
+    cp -R /code/jedi-colombia-project /home/$uname/
+done < usersAndPasswords.txt
